0f2077bc125a025cbca08ed1d9330965d2f2cb36
Colin Gaudreau <colingaudreau@wpa-5-1504.cc.umanitoba.ca>
2015-10-22 21:24:59 -0500

    Sped up the loading, and changed kernel so that it would finish in this lifetime

--- Assignment 1/sklearn_svm_demo.py
+++ Assignment 1/sklearn_svm_demo.py
@@ -8,7 +8,7 @@ import numpy as np
 import pdb
 import sys
 import time
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC, SVC
 
 def load_data(images, labels, number_desired=float('inf'), reshape=True):
 	'''
@@ -27,15 +27,9 @@ def load_data(images, labels, number_desired=float('inf'), reshape=True):
 
 		number_desired = min(number_desired, num_im)
 		loaded_images = np.empty(shape=(0,num_row,num_col), dtype=int)
-		for i in range(0,number_desired,1):
-			img = []
-			for j in range(0,num_row,1):
-				row = [struct.unpack('>B', imfile.read(1))[0] for k in range(0,28,1)]
-				img.append(row)
-			loaded_images = np.append(loaded_images, np.array([img]), axis=0)
-			if not i % 100 or i == number_desired - 1:
-				sys.stdout.write("\r%.2f%%" % (float(i+1)/float(number_desired) * 100.0))
-				sys.stdout.flush()
+		frmt = 'B' * number_desired*num_row*num_col
+		# Read all the desired bytes and store them in a numpy.ndarray
+		loaded_images = np.reshape(np.array(struct.unpack('>' + frmt, imfile.read(number_desired*num_row*num_col))), (number_desired,num_row,num_col))
 
 		print '\nFinished loading images'
 
@@ -46,7 +40,8 @@ def load_data(images, labels, number_desired=float('inf'), reshape=True):
 		lbfile.read(4) # magic number
 		num_labels = struct.unpack('>i', lbfile.read(4))[0]
 		number_desired = min(number_desired, num_labels) 
-		loaded_labels = np.array([struct.unpack('>B', lbfile.read(1))[0] for i in range(0,number_desired,1)])
+		frmt = 'B' * number_desired
+		loaded_labels = np.array(struct.unpack('>' + frmt, lbfile.read(number_desired)))
 
 		print '\nFinished loading labels'
 
@@ -66,17 +61,22 @@ def reshape_image_data(im):
 
 
 print 'Fetching training data'
-train_data, train_labels = load_data('train-images-idx3-ubyte', 'train-labels-idx1-ubyte')
+train_data, train_labels = load_data('train-images-idx3-ubyte', 'train-labels-idx1-ubyte', 6000)
 
 train_data = reshape_image_data(train_data)
-clf = SVC()
+
+# Choose type of Kernel
+# clf = LinearSVC() # choose this for linear
+# clf = SVC(kernel='rbf') # radial basis, takes a LOONNNNNNNGGGGGGGG time
+clf = SVC(kernel='poly', degree=2) # poly
+
 print 'Training data...'
 clf.fit(train_data, train_labels)
 
 del train_data, train_labels
 
 print 'Fetching test data'
-test_data, test_labels = load_data('t10k-images-idx3-ubyte', 't10k-labels-idx1')
+test_data, test_labels = load_data('t10k-images-idx3-ubyte', 't10k-labels-idx1-ubyte')
 
 test_data = reshape_image_data(test_data)
 
