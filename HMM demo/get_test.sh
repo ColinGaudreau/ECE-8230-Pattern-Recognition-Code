765c7dba37e842b4a7c061d4f6177809979d4b39
Colin Gaudreau <colingaudreau@wpa-5-1566.cc.umanitoba.ca>
2016-02-10 10:47:43 -0600

    Made small changes to bash files in HMM demo.

--- HMM demo/get_test.sh
+++ HMM demo/get_test.sh
@@ -3,18 +3,18 @@
 # of words.  This file must be placed inside 'etc' directory of an4 db.
 words=(YES NO GO STOP HELP)
 
-echo "Please input locatio you would like data, followed by [ENTER]:"
+echo "Please input location you would like data, followed by [ENTER]:"
 read datadir
 
 for word in ${words[*]}; do
 	echo Copying data from $word
 	if [ ! -d $datadir/$word ]; then
-		mkdir $datadir/$word
+		mkdir "$datadir/$word"
 	fi
 	for filename in $(cat an4_test.transcription | grep $word | grep -o -P '(?<=\().*(?=\))'); do
 		for file in $(cat an4_test.fileids | grep $filename | tr -s '[:space:]'); do
 			if [ -a "../wav/$file.raw" ]; then
-				cp ../wav/$file.raw $datadir/$word/
+				cp ../wav/$file.raw "$datadir/$word/"
 			fi
 		done
 	done
